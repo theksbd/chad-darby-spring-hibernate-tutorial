37e26ca58192e7cf5a01356f93acbdc25b7b6c69
Do Thien Hoang <theksbd@gmail.com>
2023-10-01 18:35:53 +0700

    update

---
+++ projects/first-spring-project/src/main/java/com/theksbd/firstspringproject/FirstspringprojectApplication.java
@@ -0,0 +1,13 @@
+package com.theksbd.firstspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstspringprojectApplication.class, args);
+	}
+
+}
